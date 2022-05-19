10c35dd355302c0f6d545caf00a3dc462a505894
Gürkan Bulca <53597321+gurkanbulca@users.noreply.github.com>
2022-05-19 17:45:30 +0300

    MVC final

--- Assets/Scripts/Application.cs
+++ Assets/Scripts/Application.cs
@@ -6,19 +6,18 @@ using Object = UnityEngine.Object;
 
 public class Application : MonoBehaviour
 {
-    
-    public event Action<string,Object[]> OnGameNotificationSent;
-    public event Action<string,Object[]> OnBounceNotificationSent;
+    public event Action<string, Object[]> OnGameNotificationSent;
+    public event Action<string, Object[]> OnBounceNotificationSent;
 
     public void Notify(string notificationString, params Object[] data)
     {
         if (GameNotification.Contains(notificationString))
         {
-            OnGameNotificationSent?.Invoke(notificationString,data);
+            OnGameNotificationSent?.Invoke(notificationString, data);
         }
         else if (BounceNotification.Contains(notificationString))
         {
-            OnBounceNotificationSent?.Invoke(notificationString,data);
+            OnBounceNotificationSent?.Invoke(notificationString, data);
         }
     }
 }
